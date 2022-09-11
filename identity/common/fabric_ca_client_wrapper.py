279ef525e1879e75b81e827a9d69c6856a180e66
feknall <h.fazli.k@gmail.com>
2022-09-11 11:02:43 -0400

    minor refactor

--- identity/common/fabric_ca_client_wrapper.py
+++ identity/common/fabric_ca_client_wrapper.py
@@ -4,13 +4,13 @@ from identity.base.support.utils import log_msg
 
 
 class FabricCaClientWrapper:
-    home = str
-    address = str
-    port = str
-    caname = str
-    tls_certfiles = str
-    enrollment_id = str
-    enrollment_secret = str
+    home = None
+    address = None
+    port = None
+    caname = None
+    tls_certfiles = None
+    enrollment_id = None
+    enrollment_secret = None
 
     def __init__(self, home: str,
                  address: str,
@@ -26,13 +26,13 @@ class FabricCaClientWrapper:
         self.tls_certfiles = tls_certfiles
         self.enrollment_id = enrollment_id
         self.enrollment_secret = enrollment_secret
-        self.url = "https://{enrollment_id}:{enrollment_secret}@{address}:{port}" \
-            .format(enrollment_id=self.enrollment_id, enrollment_secret=self.enrollment_secret,
-                    address=self.address, port=self.port)
+
+    def generate_url(self):
+        return f"https://{self.enrollment_id}:{self.enrollment_secret}@{self.address}:{self.port}"
 
     def enroll(self):
         command_list = ["fabric-ca-client", "enroll",
-                                       "-u", self.url,
+                                       "-u", self.generate_url(),
                                        "--caname", self.caname,
                                        "--tls.certfiles", self.tls_certfiles,
                                        "--home", self.home]
@@ -41,11 +41,11 @@ class FabricCaClientWrapper:
         if return_code == 0:
             log_msg("Enrolled successfully.")
         else:
-            log_msg(f"{return_code} Enrollment failed.")
+            log_msg(f"Enrollment failed. Code: {return_code}")
 
     def enroll_msp(self, msp):
         command_list = ["fabric-ca-client", "enroll",
-                        "-u", self.url,
+                        "-u", self.generate_url(),
                         "--caname", self.caname,
                         "--tls.certfiles", self.tls_certfiles,
                         "--home", self.home,
@@ -56,7 +56,7 @@ class FabricCaClientWrapper:
         if return_code == 0:
             log_msg("Enrolled successfully.")
         else:
-            log_msg(f"{return_code} Enrollment failed.")
+            log_msg(f"Enrollment failed. Code: {return_code}")
 
     def register(self, id_name, id_secret, id_attrs):
         command_list = ["fabric-ca-client", "register",
@@ -72,7 +72,7 @@ class FabricCaClientWrapper:
         if return_code == 0:
             log_msg("Registered successfully.")
         else:
-            log_msg(f"Enrollment failed. Code: {return_code}")
+            log_msg(f"Registration failed. Code: {return_code}")
 
     def register_fl_admin(self, id_name, id_secret):
         self.register(id_name, id_secret, "'flAdmin=true:ecert'")
